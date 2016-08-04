0ccb8b31af480bd205eba33b1bb295c0b01a91f6
Tara Willman <tara.willman@gmail.com>
2016-08-04 09:56:35 -0700

    User Story 5

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+5.times do
+  User.create!(
+  email:    Faker::Internet.email,
+  password: Faker::Internet.password
+  )
+end
+users = User.all
+
+20.times do
+  RegisteredApplication.create!(
+  name:     Faker::Lorem.word,
+  url:      Faker::Internet.url,
+  user:     users.sample
+  )
+end
+registered_applications = RegisteredApplication.all
+
+50.times do
+  Event.create!(
+  name:     Faker::Lorem.word,
+  registered_application: registered_applications.sample
+  )
+end
+
+member = User.create(
+  email: "member@example.com",
+  password: "helloworld"
+)
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{RegisteredApplication.count} registered applications created"
+puts "#{Event.count} events created"
